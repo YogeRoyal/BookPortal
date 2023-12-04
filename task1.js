63873593d7b6de04b9ae64032a8da857642e1b19
unknown <kaketiyogesh2110.com>
2023-12-04 14:48:48 +0530

    task1 completed

--- task1.js
+++ task1.js
@@ -1,82 +1,77 @@
-//show available books to users,add book,show cart
-
 const readline = require("readline-sync");
 
-let book = [
-    {
-        name:"Book1",
-        price:25,
-        status:"available",
-        quantity:12
-    },
-    {
-        name:"Book2",
-        price:20,
-        status:"available",
-        quantity:7
-    },
-    {
-        name:"Book3",
-        price:50,
-        status:"available",
-        quantity:8
-    }]
+const bookStore = [
+    { name: "Book1", price: 20, status: "available", quantity: 5 },
+    { name: "Book2", price: 30, status: "available", quantity: 8 },
+    { name: "Book3", price: 25, status: "available", quantity: 10 }
+];
 
-function displayBooks() {
-    let id = 1
+const cart = [];
+let bookIndex = 1
+function showAvailableBooks() {
     console.log(`Available Books:
 +----+--------------------+-------+----------+
 | ID |        Name        | Price | Quantity |
 +----+--------------------+-------+----------+`);
-    book.forEach(ele => {
-        console.log(`| ${id}  |        ${ele.name}       |  $${ele.price}  |     ${ele.quantity}    |`);
-        id++
+    bookStore.forEach(ele => {
+        console.log(`| ${bookIndex}  |        ${ele.name}       |  $${ele.price}  |     ${ele.quantity}   |`);
+        bookIndex++
     });
     console.log(`+----+--------------------+-------+----------+`);
 }
 
-let cart = []
-let total = 0
+function addBookToCart(bookIndex, quantity) {
+    const selectedBook = bookStore[bookIndex];
 
-function addBook(id) 
-{
-    let name = book[id-1].name
-    let price = book[id-1].price
-    book[id-1].quantity-=1
-    total += price
-    cart.push({name:name,price:price,quantity:1,total:total})
-}
+    if (selectedBook.status === "available" && selectedBook.quantity >= quantity) {
+        console.log("hi");
+        cart.push({
+            name: selectedBook.name,
+            price: selectedBook.price,
+            quantity: quantity,
+            totalPrice: selectedBook.price * quantity
+        });
 
-function showCart() {
-    if (total==0) 
-    {
-        console.log("Your Cart is Empty");
-    } else 
-    {
-        console.log(cart);
+        // Update book quantity in the store
+        selectedBook.quantity -= quantity;
+
+        console.log("\nBook added to cart successfully!");
+    } else {
+        console.log("\nBook is not available or insufficient quantity.");
     }
 }
 
-let choice = 0
+function showCart() {
+    console.log("\nShopping Cart:");
+    cart.forEach(item => {
+        console.log(`Name: ${item.name}, Price: $${item.price}, Quantity: ${item.quantity}, Total Price: $${item.totalPrice}`);
+    });
 
-while (choice!=4) 
-{
+    const totalCartValue = cart.reduce((total, item) => total + item.totalPrice, 0);
+    console.log(`\nTotal Cart Value: $${totalCartValue}`);
+}
+let choice = 0;
+while (choice != 4) {
+
     choice = readline.questionInt("\nEnter a Number:\n1.Display Books\n2.Add Books to Cart\n3.Show Cart\n4.Exit\n")
-    switch (choice) 
-    {
+    switch (choice) {
         case 1:
-            displayBooks()
+            showAvailableBooks()
             break;
         case 2:
-            let id = readline.questionInt("Add Book in cart by entering book Id: ")
-            addBook(id)
+            let bookIndex = readline.questionInt("Add Book in cart by entering book Id: ")
+            let quantity = readline.questionInt("Add the quantity of book: ")
+
+            addBookToCart(bookIndex, quantity)
+            showAvailableBooks()
             break;
         case 3:
             showCart()
+            showAvailableBooks()
             break;
         case 4:
             console.log("Thank you for shopping!!\n");
-            choice=4
+            choice = 4
             break;
         default:
             console.log("Oops,Invalid input,Enter a valid input\n");
